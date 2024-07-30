59fa08010cb0b3a29e13f6f7ecd3d891ea2d037d
juliathemelo <juliarexsteam@hotmail.com>
2024-07-30 17:43:44 -0300

    update data

--- StardewBank/src/managers/managers.controller.ts
+++ StardewBank/src/managers/managers.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import { ManagersService } from './managers.service';
-import { Manager } from './model/manager.model';
+import { ManagersService } from './managers.service.js';
+import { Manager } from './model/manager.model.js';
 
 @Controller('managers')
 export class ManagersController {
